ce9160e3309cf52996cea3c3fa91c753b442a983
vadism3d <72330747+vadism3d@users.noreply.github.com>
2022-12-18 19:32:14 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+// Окно регистации
+$('.mobile-nav-reg').click(function(){
+	$('.mobile-reg').addClass('open');
+    $('.mobile-nav').removeClass('open-open');
+})
+$('#close_popup').click(function(){
+	$('.mobile-reg').removeClass('open');
+})
+
+// 
+$('.log-reg-btn').click(function(){
+	$('.mobile-reg').addClass('open');
+    $('.mobile-nav').removeClass('open-open');
+    $('.mobile-log').removeClass('open');
+})
+$('#close_popup').click(function(){
+	$('.mobile-reg').removeClass('open');
+})
+
+// Окно входа
+$('.mobile-nav-log').click(function(){
+	$('.mobile-log').addClass('open');
+    $('.mobile-nav').removeClass('open-open');
+})
+$('#close-mobile-log').click(function(){
+	$('.mobile-log').removeClass('open');
+})
+
+// Окно навигации
+$('#open_popup').click(function(){
+	$('.mobile-nav').addClass('open-open');
+})
+
+$('#mobile-nav-close').click(function(){
+	$('.mobile-nav').removeClass('open-open');
+})
